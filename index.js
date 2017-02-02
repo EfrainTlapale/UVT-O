71a6e15ec757b78a599695a45ad5718676c699de
Efrain Tlapale <tlapalepulidoefrain@gmail.com>
2017-02-02 09:51:07 -0600

    FIX: refactor tipos de variables const y arrow functions

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+const restify = require('restify')
+const builder = require('botbuilder')
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
 // Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
+const server = restify.createServer()
+server.listen(process.env.port || process.env.PORT || 3978, (err) => {
+  if(err) console.log('ERROR: Server not running')
+  console.log(`Server listening in port: ${server}`)
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector({
+const connector = new builder.ChatConnector({
     appId: 'aa4984b4-2f36-4be4-864d-1b5d013cbc89',
     appPassword: 'dawCxefqAiqrzPOeyX0SF6k'
 });
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+const bot = new builder.UniversalBot(connector)
+server.post('/api/messages', connector.listen())
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-var apiai = require('apiai')
-var ai = apiai('b0fb9d9ce53641a1aaddce07ada5109a');
-var respuestas = require('./respuestas');
+const apiai = require('apiai')
+const ai = apiai('b0fb9d9ce53641a1aaddce07ada5109a')
+var respuestas = require('./respuestas')
 
-bot.dialog('/', function (session) {
-  var request = ai.textRequest(session.message.text, {
+bot.dialog('/', (session) => {
+  const request = ai.textRequest(session.message.text, {
     sessionId: 'something'
   });
 
-  request.on('response', function (response) {
+  request.on('response', (response) => {
     switch (response.result.action) {
       case 'input.welcome':
         respuestas.saludo(session, response)
@@ -52,13 +53,9 @@ bot.dialog('/', function (session) {
     }
   });
 
-  request.on('error', function(error) {
-    console.log(error);
-  });
+  request.on('error', (error) => {
+    console.log(error)
+  })
 
-  request.end();
-});
-
-var handleResponse = function(session, response) {
-  session.send(response.result.fulfillment.speech)
-}
+  request.end()
+})
