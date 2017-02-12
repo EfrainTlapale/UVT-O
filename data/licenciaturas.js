6b205d81a976a94404be5924f1b3035c6fd2fd1a
Efrain Tlapale <tlapalepulidoefrain@gmail.com>
2017-02-12 16:59:18 -0600

    ADD: eslint config and refactor standard config typos

--- data/licenciaturas.js
+++ data/licenciaturas.js
@@ -1,6 +1,6 @@
 module.exports = [
   {nombre: 'Ingenieria en Sistemas', description: 'programacion, redes y proyectos'},
-  {nombre: 'Psicoloíga', descripcion:'algo sobre psicologia'},
+  {nombre: 'Psicoloíga', descripcion: 'algo sobre psicologia'},
   {nombre: 'Lenguas Modernas', descripcion: 'algo de lenguas modernas'},
   {nombre: 'Diseño y Comunicación Digital', descripcion: 'algo de DCD'},
   {nombre: 'Arquitectura', descripcion: 'algo de Arquitectura'},
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const restify = require('restify')
 const builder = require('botbuilder')
 
-//=========================================================
+// =========================================================
 // Bot Setup
-//=========================================================
+// =========================================================
 
 // Setup Restify Server
 const server = restify.createServer()
 server.listen(process.env.port || process.env.PORT || 3978, (err) => {
-  if(err) console.log('ERROR: Server not running')
+  if (err) console.log('ERROR: Server not running')
   console.log(`Server listening in port: ${server}`)
-});
+})
 
 // Create chat bot
 const connector = new builder.ChatConnector({
-    appId: 'aa4984b4-2f36-4be4-864d-1b5d013cbc89',
-    appPassword: 'dawCxefqAiqrzPOeyX0SF6k'
-});
+  appId: 'aa4984b4-2f36-4be4-864d-1b5d013cbc89',
+  appPassword: 'dawCxefqAiqrzPOeyX0SF6k'
+})
 const bot = new builder.UniversalBot(connector)
 server.post('/api/messages', connector.listen())
 
-//=========================================================
+// =========================================================
 // Bots Dialogs
-//=========================================================
+// =========================================================
 
 const apiai = require('apiai')
 const ai = apiai('b0fb9d9ce53641a1aaddce07ada5109a')
@@ -31,27 +31,26 @@ var respuestas = require('./respuestas')
 bot.dialog('/', (session) => {
   const request = ai.textRequest(session.message.text, {
     sessionId: 'something'
-  });
+  })
 
   request.on('response', (response) => {
     switch (response.result.action) {
       case 'input.welcome':
         respuestas.saludo(session, response)
-        break;
+        break
       case 'infolicenciatura':
         respuestas.licenciaturas(session, response)
-        break;
+        break
       case 'identificacion':
         respuestas.identificacion(session, response)
-        break;
+        break
       case 'estadoanimo':
         respuestas.estadoanimo(session, response)
-        break;
+        break
       default:
-      respuestas.error(session, response)
-
+        respuestas.error(session, response)
     }
-  });
+  })
 
   request.on('error', (error) => {
     console.log(error)
