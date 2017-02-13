6d1d36b530aad382e20079886f99fe8447a271c9
Efrain Tlapale <tlapalepulidoefrain@gmail.com>
2017-02-12 19:43:14 -0600

    ADD: client react project and initial setup

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='mdl-layout__header mdl-color--green-500'>
+      <div className='mdl-layout__header-row'>
+        <span className='mdl-layout-title'>UVT-O Admin Panel</span>
+        <div className='mdl-layout-spacer' />
+        <nav className='mdl-navigation mdl-layout--large-screen-only'>
+          <a className='mdl-navigation__link' href=''>Link</a>
+          <a className='mdl-navigation__link' href=''>Link</a>
+          <a className='mdl-navigation__link' href=''>Link</a>
+          <a className='mdl-navigation__link' href=''>Link</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
