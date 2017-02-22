ada9f07d6905077d2c38e56ca4985fdc94d6c17e
Efrain Tlapale <tlapalepulidoefrain@gmail.com>
2017-02-22 16:20:09 -0600

    FIX: todas las ligas a las licenciaturas existentes

--- data/licenciaturas.js
+++ data/licenciaturas.js
@@ -1,8 +1,8 @@
 module.exports = [
-  {nombre: 'Ingenieria en Sistemas', description: 'programacion, redes y proyectos', link: 'google.com'},
-  {nombre: 'Psicoloíga', descripcion: 'algo sobre psicologia', link: ''},
-  {nombre: 'Lenguas Modernas', descripcion: 'algo de lenguas modernas', link: ''},
-  {nombre: 'Diseño y Comunicación Digital', descripcion: 'algo de DCD', link: ''},
-  {nombre: 'Arquitectura', descripcion: 'algo de Arquitectura', link: ''},
-  {nombre: 'Derecho', descripcion: 'algo de Derecho', link: ''}
+  {nombre: 'Ingenieria en Sistemas', description: 'programacion, redes y proyectos', link: 'http://univalletlax.edu.mx/oferta-educativa/licenciaturas/ingenieria-en-sistemas-computacionales/'},
+  {nombre: 'Psicoloíga', descripcion: 'algo sobre psicologia', link: 'http://univalletlax.edu.mx/oferta-educativa/licenciaturas/licenciatura-en-psicologia/'},
+  {nombre: 'Lenguas Modernas', descripcion: 'algo de lenguas modernas', link: 'http://univalletlax.edu.mx/oferta-educativa/licenciaturas/licenciatura-en-lenguas-modernas/'},
+  {nombre: 'Diseño y Comunicación Digital', descripcion: 'algo de DCD', link: 'http://univalletlax.edu.mx/oferta-educativa/licenciaturas/licenciatura-en-diseno-y-comunicacion-digital/'},
+  {nombre: 'Arquitectura', descripcion: 'algo de Arquitectura', link: 'http://univalletlax.edu.mx/oferta-educativa/licenciaturas/licenciatura-en-arquitectura/'},
+  {nombre: 'Derecho', descripcion: 'algo de Derecho', link: 'http://univalletlax.edu.mx/oferta-educativa/licenciaturas/licenciatura-en-derecho/'}
 ]
