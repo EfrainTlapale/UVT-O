e2727ebe332fc742691b923756fe34c1a0e478cd
Efrain Tlapale <tlapalepulidoefrain@gmail.com>
2017-03-13 03:22:20 -0600

    ADD: api endpoint for logs

--- api/index.js
+++ api/index.js
@@ -50,4 +50,24 @@ router.get('/protected', jwtcheck, (req, res) => {
   res.json({secret: 'data', ultra: 'topSecret'})
 })
 
+router.get('/logs', (req, res) => {
+  server.models.logs.find({}).exec((err, logs) => {
+    if (err) {
+      res.status(400).json({success: false, err})
+    } else {
+      res.json({logs})
+    }
+  })
+})
+
+// router.get('/intentions', (req, res) => {
+//   server.models.logs.query('Select distinct intention from logs;', (err, intentions) => {
+//     if (err) {
+//       res.status(400).json({err})
+//     } else {
+//       res.json({intentions})
+//     }
+//   })
+// })
+
 module.exports = router
